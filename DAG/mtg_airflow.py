0af6604aa73a299d6830d950cfbbe81944a1677f
Ole <ole.reimers@gmx.de>
2022-12-02 20:56:49 +0100

    Doku und fixes

--- DAG/mtg_airflow.py
+++ DAG/mtg_airflow.py
@@ -106,7 +106,9 @@ FROM
     cards c
     JOIN foreign_cards f ON (c.id = f.cardid)
 WHERE
-    f.language = "German";
+    f.language = "German"
+    AND c.partition_year = {{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}} and c.partition_month = {{ macros.ds_format(ds, "%Y-%m-%d", "%m")}} and c.partition_day = {{ macros.ds_format(ds, "%Y-%m-%d", "%d")}}
+    AND f.partition_year = {{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}} and f.partition_month = {{ macros.ds_format(ds, "%Y-%m-%d", "%m")}} and f.partition_day = {{ macros.ds_format(ds, "%Y-%m-%d", "%d")}};
 '''
 
 ##
--- expressWebServer/db/conn.js
+++ expressWebServer/db/conn.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 var CardsSchema = new Schema(
   {
     name: { type: String },
-    id: { type: String },
+    id: { type: Schema.Types.Mixed },
     imageUrl: { type: String },
   },
   { versionKey: false }
@@ -13,7 +13,11 @@ mongoose.connect('mongodb://mongodb-container:27017/MTGCards');
 var model = mongoose.model('mtgCards', CardsSchema, 'mtgCards');
 
 async function getCards(name) {
-  return await model.find({ name: new RegExp(name, 'i') }).exec();
+  id = parseInt(name);
+  if (!id || typeof id == NaN) {
+    id = -1;
+  }
+  return await model.find({$or: [{ name: new RegExp(name, 'i') }, { id: id }]}).exec();
 }
 
 module.exports = {
